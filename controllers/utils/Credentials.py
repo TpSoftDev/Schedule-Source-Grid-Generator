4364a45b237d3c7b6f02b0a30a12af4ad7595062
benjamintowle04 <benjamintowle04@gmail.com>
2024-12-23 11:29:05 -0600

    Documentation added for grid generator

--- controllers/utils/Credentials.py
+++ controllers/utils/Credentials.py
@@ -1,3 +1,23 @@
+"""
+credentials_manager.py
+
+OVERVIEW:
+    This module represents the credentials for user authentication needed to access the
+    external API of schedule source. In order to get an api token and session id, the program must
+    use these credentials to authenticate the user in schedule source.
+
+FUNCTIONS AND CLASSES:
+    - class Credentials:
+        Represents a user's authentication credentials with fields for code, username, and password.
+
+    - def load_creds():
+        Creates and returns an instance of the `Credentials` class with hardcoded authentication
+        information. Also logs the loaded credentials to the console for debugging purposes.
+
+USAGE:
+    This class is used by the base_auth.py
+"""
+
 class Credentials:
           def __init__(self, code, user, password):
                     self.code = code
@@ -6,5 +26,4 @@ class Credentials:
 
 def load_creds():
           creds = Credentials("isu", "btowle04", "6269")
-          print(f"Loaded credentials: code={creds.code}, user={creds.user}, password={creds.password}")
           return creds
